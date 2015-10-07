73a884de2225a4b04c627162ba91e11b0fe7927c
Derek Chan <derekchan916@gmail.com>
2015-10-06 22:09:46 -0400

    Remove bug.

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,10 +1,8 @@
-require "byebug"
-
 class LeadersController < ActionController::Base
   def create
     leader = Leader.new(leader_params)
     user = leader.find_by(name: leader.name)
-    
+
     if user && (user.score < leader.name)
       user.update!(score: leader.score)
       @leaders = Leader.all.order(score: :desc).limit(6)
