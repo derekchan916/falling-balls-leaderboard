dedf7ec919aa10827624863439cf41e00ac0fb15
Derek Chan <derekchan916@gmail.com>
2015-10-06 22:06:31 -0400

    Edit the way scores are added.

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,20 +1,23 @@
+require "byebug"
+
 class LeadersController < ActionController::Base
-
-
   def create
     leader = Leader.new(leader_params)
-    if leader.name == "Enter your name" || leader.name.strip.empty?
-      leader.name = "Anonymous"
+    user = leader.find_by(name: leader.name)
+    
+    if user && (user.score < leader.name)
+      user.update!(score: leader.score)
+      @leaders = Leader.all.order(score: :desc).limit(6)
+    else
+      leader.save
+      @leaders = Leader.all.order(score: :desc).limit(6)
     end
-
-    leader.save
-    @leaders = Leader.all.order(score: :desc).limit(10)
 
     render json: @leaders
   end
 
   def index
-    @leaders = Leader.all.order(score: :desc).limit(10)
+    @leaders = Leader.all.order(score: :desc).limit(6)
 
     render json: @leaders
   end
